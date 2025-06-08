1e1cb9f8b35b8655e38ae577806500fd094ae3c7
Stonygeist123 <yunus.ibrahim.hassen@gmail.com>
2025-06-08 18:45:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delta
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
